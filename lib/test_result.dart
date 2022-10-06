c6716c6c970ea25a57d669c975c63ba1987341b7
bgomez-signa <b.gomez@signa-sportsunited.com>
2022-10-06 16:22:18 +0200

    GREEN: modify TestResult to track failures

--- lib/test_result.dart
+++ lib/test_result.dart
@@ -1,9 +1,12 @@
 class TestResult {
   int runCount;
+  int failureCount;
 
-  TestResult({this.runCount = 0});
+  TestResult({this.runCount = 0, this.failureCount = 0});
 
-  String summary() => "$runCount run, 0 failed";
+  String summary() => "$runCount run, $failureCount failed";
 
   void testStarted() => runCount++;
+
+  void testFailed() => failureCount++;
 }
