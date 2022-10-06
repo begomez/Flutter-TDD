f80c5cf8f3d0f7e3885ad981548f665157dc38b5
bgomez-signa <b.gomez@signa-sportsunited.com>
2022-10-06 10:04:26 +0200

    GREEN: add setUp property

--- lib/was_run.dart
+++ lib/was_run.dart
@@ -4,8 +4,9 @@ import 'package:junit/test_case.dart';
 @aCustomReflectable
 class WasRun extends TestCase {
   bool wasRun;
+  bool wasSetUp;
 
-  WasRun(String name, {this.wasRun = false}) : super(name);
+  WasRun(String name, {this.wasRun = false, this.wasSetUp = true}) : super(name);
 
   void testMethod() {
     wasRun = true;
